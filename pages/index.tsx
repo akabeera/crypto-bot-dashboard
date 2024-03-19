3003b173ea112eb28255cb53a0bc56513b109e06
akabeera <avenash.kabeera01@gmail.com>
2024-03-19 12:29:55 +0800

    feat: edit layout for main screen with responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,24 +42,30 @@ export default function Home() {
 
   return (
     
-    <main className={`flex flex-col justify-between p-4 ${inter.className}`}>
-      <div className="z-10 w-full items-center justify-between font-mono">
-        <p className="text-3xl left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Crypto-Bot Performance Dashboard
-        </p>
+    <main className={`flex flex-row  ${inter.className}`}>
+      <div className="lg:basis-1/6 md:flex-none"></div>
+
+      <div className="lg:basis-4/6 justify-between p-4 flex-col">
+        <div className="z-10 w-full items-center justify-between font-mono">
+          <p className="text-3xl left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Crypto-Bot Performance Dashboard
+          </p>
+        </div>
+
+        <div>
+          <div className="w-full">
+            <CurrentHoldings/>
+          </div>
+          <div>
+            <ScatterChart title="Historical Performance" tickerLists={tickerLists} tickersSellOrders={tickersSellOrders} />
+          </div>
+          <div>
+            <ProfitsLineChart title="Profits Over Time" tickerLists={tickerLists} tickersSellOrders={tickersSellOrders} />
+          </div>
+        </div>
       </div>
 
-      <div>
-        <div className="w-full">
-          <CurrentHoldings/>
-        </div>
-        <div>
-          <ScatterChart title="Historical Performance" tickerLists={tickerLists} tickersSellOrders={tickersSellOrders} />
-        </div>
-        <div>
-          <ProfitsLineChart title="Profits Over Time" tickerLists={tickerLists} tickersSellOrders={tickersSellOrders} />
-        </div>
-      </div>
+      <div className="lg:flex-1/6 md:flex-none"></div>
 
     </main>
   );
