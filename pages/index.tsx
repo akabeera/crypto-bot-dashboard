6742837b3a03974c32e13f5f4f6c6287fd0280c6
akabeera <avenash.kabeera01@gmail.com>
2024-03-19 15:45:44 +0800

    feat: maing layout responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,18 +42,18 @@ export default function Home() {
 
   return (
     
-    <main className={`flex flex-row  ${inter.className}`}>
-      <div className="lg:basis-1/6 md:flex-none"></div>
+    <main className={`flex flex-row ${inter.className}`}>
+      <div className="flex-grow"></div>
 
-      <div className="lg:basis-4/6 justify-between p-4 flex-col">
-        <div className="z-10 w-full items-center justify-between font-mono">
-          <p className="text-3xl left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+      <div className="flex flex-col justify-between p-4">
+        <div className="items-center justify-between font-mono">
+          <p className="flex text-3xl justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit">
             Crypto-Bot Performance Dashboard
           </p>
         </div>
 
         <div>
-          <div className="w-full">
+          <div>
             <CurrentHoldings/>
           </div>
           <div>
@@ -65,7 +65,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="lg:flex-1/6 md:flex-none"></div>
+      <div className="flex-grow"></div>
 
     </main>
   );
